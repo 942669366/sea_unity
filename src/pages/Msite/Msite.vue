97bd35a7da926f98dcb11801c069353d3a27fae6
asus <942669366@qq.com>
2019-03-18 17:41:04 +0800

    初次开发

---
+++ src/pages/Msite/Msite.vue
@@ -0,0 +1,582 @@
+<template>
+	<section class="msite">
+		<!--首页头部-->
+		<HeaderTop title="海团">
+			<span class="header_search" slot="left">
+				<i class = "iconfont icon-sousuo"></i>
+			</span>
+			<span class="header_login" slot="right">
+				<span class="header_login_text">登陆|注册</span>
+			</span>
+		</HeaderTop>
+		<!--首页导航-->
+		<nav class="msite_nav">
+			<div class="swiper-container">
+				<div class="swiper-wrapper">
+					<div class="swiper-slide">
+						  <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+					</div>
+					<div class="swiper-slide">
+            <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+              <a href="javascript:" class="link_to_food">
+                <div class="food_container">
+                  <img src="../../../build/logo.png">
+                  <span>水果</span>
+                </div>
+						  </a>
+					</div>
+				</div>
+				
+				<!-- 如果需要分页器 -->
+				<div class="swiper-pagination"></div>
+
+        <div class="swiper-scrollbar">
+        </div>
+				
+			</div>
+		</nav>
+    <div class="mht_list">
+      <div class="move">电影</div>
+      <div class="hotel">酒店</div>
+      <div class="tickets">火车票</div>
+    </div>
+    <div class="foot_list">
+      <ul>
+        <li class="store_li">
+          <div class="store_logo">
+            <img src="../../../build/logo.png"/>
+          </div>
+          <div class="store_massage">
+            <div class="store_title">
+              <a href="javascript:" class="stroe_name">百位果园</a>
+            </div>
+            <div>
+              <div class="grage">
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+              </div>
+              <div class="sales">
+                <span>月销量</span>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="store_li">
+          <div class="store_logo">
+            <img src="../../../build/logo.png"/>
+          </div>
+          <div class="store_massage">
+            <div class="store_title">
+              <a href="javascript:" class="stroe_name">百位果园</a>
+            </div>
+            <div>
+              <div class="grage">
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+              </div>
+              <div class="sales">
+                <span>月销量</span>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="store_li">
+          <div class="store_logo">
+            <img src="../../../build/logo.png"/>
+          </div>
+          <div class="store_massage">
+            <div class="store_title">
+              <a href="javascript:" class="stroe_name">百位果园</a>
+            </div>
+            <div>
+              <div class="grage">
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+              </div>
+              <div class="sales">
+                <span>月销量</span>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="store_li">
+          <div class="store_logo">
+            <img src="../../../build/logo.png"/>
+          </div>
+          <div class="store_massage">
+            <div class="store_title">
+              <a href="javascript:" class="stroe_name">百位果园</a>
+            </div>
+            <div>
+              <div class="grage">
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+              </div>
+              <div class="sales">
+                <span>月销量</span>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="store_li">
+          <div class="store_logo">
+            <img src="../../../build/logo.png"/>
+          </div>
+          <div class="store_massage">
+            <div class="store_title">
+              <a href="javascript:" class="stroe_name">百位果园</a>
+            </div>
+            <div>
+              <div class="grage">
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+              </div>
+              <div class="sales">
+                <span>月销量</span>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="store_li">
+          <div class="store_logo">
+            <img src="../../../build/logo.png"/>
+          </div>
+          <div class="store_massage">
+            <div class="store_title">
+              <a href="javascript:" class="stroe_name">百位果园</a>
+            </div>
+            <div>
+              <div class="grage">
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+              </div>
+              <div class="sales">
+                <span>月销量</span>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="store_li">
+          <div class="store_logo">
+            <img src="../../../build/logo.png"/>
+          </div>
+          <div class="store_massage">
+            <div class="store_title">
+              <a href="javascript:" class="stroe_name">百位果园</a>
+            </div>
+            <div>
+              <div class="grage">
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+              </div>
+              <div class="sales">
+                <span>月销量</span>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="store_li">
+          <div class="store_logo">
+            <img src="../../../build/logo.png"/>
+          </div>
+          <div class="store_massage">
+            <div class="store_title">
+              <a href="javascript:" class="stroe_name">百位果园</a>
+            </div>
+            <div>
+              <div class="grage">
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+              </div>
+              <div class="sales">
+                <span>月销量</span>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="store_li">
+          <div class="store_logo">
+            <img src="../../../build/logo.png"/>
+          </div>
+          <div class="store_massage">
+            <div class="store_title">
+              <a href="javascript:" class="stroe_name">百位果园</a>
+            </div>
+            <div>
+              <div class="grage">
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+              </div>
+              <div class="sales">
+                <span>月销量</span>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="store_li">
+          <div class="store_logo">
+            <img src="../../../build/logo.png"/>
+          </div>
+          <div class="store_massage">
+            <div class="store_title">
+              <a href="javascript:" class="stroe_name">百位果园</a>
+            </div>
+            <div>
+              <div class="grage">
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+              </div>
+              <div class="sales">
+                <span>月销量</span>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="store_li">
+          <div class="store_logo">
+            <img src="../../../build/logo.png"/>
+          </div>
+          <div class="store_massage">
+            <div class="store_title">
+              <a href="javascript:" class="stroe_name">百位果园</a>
+            </div>
+            <div>
+              <div class="grage">
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+              </div>
+              <div class="sales">
+                <span>月销量</span>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="store_li">
+          <div class="store_logo">
+            <img src="../../../build/logo.png"/>
+          </div>
+          <div class="store_massage">
+            <div class="store_title">
+              <a href="javascript:" class="stroe_name">百位果园</a>
+            </div>
+            <div>
+              <div class="grage">
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+                <img src="../../../build/logo.png"/>
+              </div>
+              <div class="sales">
+                <span>月销量</span>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+    
+	</section>
+</template>
+
+<script>
+	import HeaderTop from '../../components/HeaderTop/HeaderTop.vue'
+	import Swiper from 'swiper'
+	import 'swiper/dist/css/swiper.min.css'
+	export default {
+		mounted() {
+			new Swiper('.swiper-container',{
+				loop:true,
+				pagination:{
+					el:'.swiper-pagination',
+				},
+			})
+		},
+		components:{
+			HeaderTop
+		}
+	}
+</script>
+
+
+<style lang="stylus" rel="stylesheet/stylus">
+   @import "../../common/stylus/mixins.styl"
+  .header
+      background-color #02a774
+      position fixed
+      z-index 100
+      left 0
+      top 0
+      width 100%
+      height 45px
+    .swiper-wrapper
+      width 100%
+      height 200px
+      margin-top 46px
+      
+      .swiper-slide
+        width 100%
+        height 165px
+
+        background-color c2c2c2
+        .link_to_food
+          float left
+          margin-left 26px
+          margin-top 10px
+          .food_container
+            height 40px
+            width 40px
+            text-align center
+            font-size 12px
+            span
+              margin-left 9px 
+              float left 
+            img 
+              width 20px
+              height 20px
+  .mht_list
+    width 100%
+    background-color red
+    .move
+      font-size 10px
+      margin-left 10px
+      float left
+      text-align center
+      line-height 25px
+      height 25px
+      width  80px
+      background-color #efefef
+    .hotel
+      font-size 10px
+      margin-left 10px
+      float left
+      text-align center
+      height 25px
+      line-height 25px
+      width  80px
+      background-color #efefef
+    .tickets
+      font-size 10px
+      margin-left 10px
+      float left
+      text-align center
+      height 25px
+      line-height 25px
+      width  80px
+      background-color #efefef
+  .store_li
+    float left
+    width 100%
+    height 50px
+    margin-top 5px
+    .store_logo
+      text-align center
+      line-height 50px
+      float left
+      width 50px
+      height 50px
+      img
+        width 40px
+        height 40px
+    .store_massage
+      margin-left 1px
+      width 238px
+      height 50px
+      float left
+      .store_title
+        margin-left  1px
+        margin-top 1px
+        
+      a
+        text-decoration none 
+        font-size 20px
+        color #8c8c8c
+      .grage
+        height 20px
+        width 100px
+        margin-top 5px
+        img
+          line-height 15px
+          margin-left 1px
+          float left
+          width 12px
+          height 12px
+      .sales
+        font-size 10px 
+        line-height 20px
+        width 80px
+        height 20px
+        margin-left 110px
+        margin-top -20px
+        float left
+      
+</style>
